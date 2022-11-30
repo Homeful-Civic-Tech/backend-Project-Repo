b83cf3b64a1b852900e6befea21b76c2557fdc18
Aneika Nanton <101756732+Aneika11@users.noreply.github.com>
2022-11-30 16:38:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,22 @@ app.use(express.json());
 app.use('/user',userRoutes);
 app.use('/rooms',roomsRoutes)
 
+app.get('/shelters', async(req,res) => {
+    const getshelters = await  pool.query('SELECT * FROM shelters').then(results => {return results.rows})
+  if(getshelters){
+     res.status(200).json(getshelters)
+  } else {
+    res.sendStatus(404)
+  } 
+});
+
+
+
+
+// all other paths
+app.all('*', (req, res)=>{
+    res.send('Path does not exist')
+});
 
 app.listen(port, () => {
     console.log(`listening at port: ${port}`);
